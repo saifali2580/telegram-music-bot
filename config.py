4aec367e6219b2d6aa6253166531590576ee5d7f
DowzG <87517117+saifali2580@users.noreply.github.com>
2026-05-22 17:10:09 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,6 @@
-import os
-
-# بيانات الاتصال الأساسية
-API_ID = int(os.getenv("API_ID", "1234567")) # ضع الـ API ID الافتراضي هنا أو بالسيرفر
-API_HASH = os.getenv("API_HASH", "your_api_hash")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")
-STRING_SESSION = os.getenv("STRING_SESSION", "your_string_session")
-
-# آيدي المطور الأساسي للبوت (يتم سحبه تلقائياً عند التنصيب)
-OWNER_ID = int(os.getenv("OWNER_ID", "your_telegram_id")) # ضع آيديك هنا كافتراضي
+API_ID = 123456
+API_HASH = "your_api_hash"
+BOT_TOKEN = "bot_token"
+STRING_SESSION = "session_string"
+OWNER_ID = 123456789
+BOT_NAME = "الموسيقار"
