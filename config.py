b961f92894dff5b3112647ab585a2902c60e2d7e
DowzG <87517117+saifali2580@users.noreply.github.com>
2026-05-22 15:42:32 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+API_ID = int(os.getenv("API_ID", "1234567")) # ضع الـ API ID هنا أو بالسيرفر
+API_HASH = os.getenv("API_HASH", "your_api_hash")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")
+STRING_SESSION = os.getenv("STRING_SESSION", "your_string_session")
+
